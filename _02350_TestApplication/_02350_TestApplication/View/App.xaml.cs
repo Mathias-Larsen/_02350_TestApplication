4a13b2d4b0a42ea187c65dad0dbbe0a7c1c73196
Mathias-Larsen <Mathias.e.larsen@gmail.com>
2013-10-15 01:07:21 +0200

    Added the project + a bunch of code

---
+++ _02350_TestApplication/_02350_TestApplication/View/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using _02350_TestApplication.View;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _02350_TestApplication.View
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
