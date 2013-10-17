9b013c665afc9629d874df2cec2e1fe6612fda1e
Mathias-Larsen <Mathias.e.larsen@gmail.com>
2013-10-17 11:04:49 +0200

    Fixed main window

--- _02350_TestApplication/_02350_TestApplication/ViewModel/MainViewModel.cs
+++ _02350_TestApplication/_02350_TestApplication/ViewModel/MainViewModel.cs
@@ -31,8 +31,24 @@ namespace _02350_TestApplication.ViewModel
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
+        /// 
+
+        public ObservableCollection<ClassBox> ClassBoxes { get; set; }
+        public ObservableCollection<Edge> Edges { get; set; }
+        public ObservableCollection<ClassBox> SelectedClassBox { get; set; }
+
         public MainViewModel()
         {
+            //SelectedClassBox = new ObservableCollection<ClassBox>();
+
+            ClassBoxes = new ObservableCollection<ClassBox>()
+            {
+                new ClassBox() { X=30, Y=40, Width=80, Height=80 }, // jeg fatter overhoved ikke det her med komma og semikolon til sidst??  @mathias
+                new ClassBox() { X=140, Y=230, Width=80, Height=80}
+            };
+
+
+
             ////if (IsInDesignMode)
             ////{
             ////    // Code runs in Blend --> create design time data.
